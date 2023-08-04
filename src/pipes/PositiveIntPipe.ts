e2a07a2232eb95f8b77af715a9cb92829aea387c
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-08-04 17:41:42 +0300

    small fix with pipe

--- src/pipes/PositiveIntPipe.ts
+++ src/pipes/PositiveIntPipe.ts
@@ -4,7 +4,7 @@ import { PipeTransform, BadRequestException, Injectable } from "@nestjs/common";
 export class PositiveIntPipe implements PipeTransform {
     transform(value: any) {
         const intValue = parseInt(value, 10);
-        if (intValue <= 0) {
+        if (intValue < 0) {
             throw new BadRequestException("Invalid ID (positive number is expected)");
         }
         if (isNaN(intValue)) {
